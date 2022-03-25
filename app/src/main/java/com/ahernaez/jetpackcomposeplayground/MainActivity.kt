7f638f5bb78d0fa7580d5b2e5408d04e1569069c
Anthony Hernaez <hernaezanthony06@gmail.com>
2022-03-25 21:00:14 +0800

    Added files

---
+++ app/src/main/java/com/ahernaez/jetpackcomposeplayground/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.ahernaez.jetpackcomposeplayground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Checkbox
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ahernaez.jetpackcomposeplayground.ui.theme.JetpackComposePlaygroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposePlaygroundTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting(name = "world")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposePlaygroundTheme {
+        Greeting("Android")
+    }
+}
